f3f9b01ccd3e5c4f96e0d4552a0e41d441037fd0
Peter Mgembe <peterrobsson@gmail.com>
2023-10-12 09:05:12 +0300

    removed brackets from DEFAULT directive

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Create a table user with four columns id, email, name and country
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT('US')
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
