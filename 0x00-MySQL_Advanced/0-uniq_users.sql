f7dd298ec92ecefc8ba1a247afebf857bafff5bd
Peter Mgembe <peterrobsson@gmail.com>
2023-10-12 10:24:24 +0300

    reverted the previous changes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- SQL create a users table with three columns
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT INDEX,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
+    UNIQUE INDEX id_UNIQUE (id)
 );
