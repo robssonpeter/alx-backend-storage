09373e8b7c88173ebdcba921f621f9f38827b48e
Peter Mgembe <peterrobsson@gmail.com>
2023-10-12 10:23:42 +0300

    shifted the index to the respective id column

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- SQL create a users table with three columns
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT INDEX,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
-    UNIQUE INDEX id_UNIQUE (id)
 );
