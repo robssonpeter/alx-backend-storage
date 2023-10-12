ae500139b41c0e863822302c55b95df42de386fc
Peter Mgembe <peterrobsson@gmail.com>
2023-10-12 15:39:01 +0300

    solved the question 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
 -- Get bands with Glam rock style
-SELECT band_name, split - formed AS lifespan
+SELECT  
+    band_name, 
+    IF(split IS NULL, 2022 - formed, split - formed) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
