b49d6d16e540535d4dd4046d1c165c40a47cb480
Peter Mgembe <peterrobsson@gmail.com>
2023-10-14 18:31:24 +0300

    tried to solve the 7th question for find the average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,7 +2,9 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SELECT DISTINCT AVG(score) INTO @average FROM corrections WHERE user_id = @user_id;
+    SELECT project_id INTO @project FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO @average FROM corrections
+        WHERE project_id = @project AND user_id = @user_id;
     UPDATE users SET average_score = @average WHERE id = user_id;
 END //
 DELIMITER ;
