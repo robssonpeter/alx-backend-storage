65b31479bc17d33eef57abd57222804c09e2454c
Peter Mgembe <peterrobsson@gmail.com>
2023-10-14 17:34:33 +0300

    trying to solve question 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,13 @@
 -- Script for Average Score Computation Procedure
 DELIMITER //
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SELECT AVG(score) INTO @average FROM corrections WHERE user_id = @user_id;
-    UPDATE users SET average_score = @average WHERE id = user_id;
+    DECLARE average_score DECIMAL(10, 2);
+    
+    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
+    
+    UPDATE users SET average_score = average_score WHERE id = user_id;
 END //
+
 DELIMITER ;
