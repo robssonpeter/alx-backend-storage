9b4a34b7a06329c4d8d3ec3b3c6ade57d72c9391
Peter Mgembe <peterrobsson@gmail.com>
2023-10-14 18:17:51 +0300

    tried to solve question 6 for adding bonus entrie to the corrections table

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,4 +2,13 @@
 DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name TEXT, IN score FLOAT)
 BEGIN
-    
+    -- check if the project exists
+    SELECT id INTO @project FROM projects WHERE name = project_name;
+    -- if not existing the create a new project
+    IF @project IS NULL THEN
+        INSERT INTO projects (`name`) VALUES (project_name);
+        SELECT LAST_INSERT_ID() INTO @project;
+    END IF;
+    INSERT INTO corrections (`user_id`, `project_id`, `score`) VALUES (user_id, @project, score);
+END //
+DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,8 @@
 -- Script for Average Score Computation Procedure
 DELIMITER //
-
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average_score DECIMAL(10, 2);
-    
-    SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
-    
-    UPDATE users SET average_score = average_score WHERE id = user_id;
+    SELECT DISTINCT AVG(score) INTO @average FROM corrections WHERE user_id = @user_id;
+    UPDATE users SET average_score = @average WHERE id = user_id;
 END //
-
 DELIMITER ;
