ea5a8e7fe6a1c2d8e366a6ed03dd6acccbdb3634
Peter Mgembe <peterrobsson@gmail.com>
2023-10-15 04:41:49 +0300

    sovled the 9th question

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,3 @@
 -- Index idx_name_first_score on the table 
 -- and the first letter of name and the score
-USE holberton;
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
