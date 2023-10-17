5f709e6f6757845e016e874dea783bc34f9f8176
Peter Mgembe <peterrobsson@gmail.com>
2023-10-17 13:53:07 +0300

    changed the connection db link from localhost to an ip

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://localhost:27017")
+    client = MongoClient("mongodb://127.0.0.1:27017")
 
     print(f"{client.logs.nginx.count()} logs")
     print("Methods:")
