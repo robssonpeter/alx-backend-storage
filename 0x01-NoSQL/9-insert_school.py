8bf8229f5883a945d42b806bea36561ab821a4ac
Peter Mgembe <peterrobsson@gmail.com>
2023-10-17 12:43:27 +0300

    removed the json encoder

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@ import json
 
 def insert_school(mongo_collection, **kwargs):
     """ THe function that will insert a new entry to db """
-    inserted = mongo_collection.insert_one(json.dumps(kwargs))
+    inserted = mongo_collection.insert_one(kwargs)
     return inserted.inserted_id
